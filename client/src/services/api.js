6efa38a0aa5963d1b99c336660cd85e577fa5084
Shubham Bharti <shubhambharti711@gmail.com>
2025-07-03 22:40:36 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://finance-tracker-server-pgut.onrender.com",
+    "https://finance-tracker-server-pgut.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
