0e2946d9ef76ba2e41a7a04227f0be0202053d46
Shubham Bharti <shubhambharti711@gmail.com>
2025-07-04 12:25:42 +0530

    Porojection video upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <>
       <Navbar />
-      {/* Tailwind container instead of MUI */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <Routes>
           <Route path="/login" element={<Login />} />
